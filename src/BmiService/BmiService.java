c583011035e63119a828188ef8e4a47c94ad629c
First test <vitik03.90@gmail.com>
2024-06-10 23:31:56 +0300

    Initial commit

---
+++ src/BmiService/BmiService.java
@@ -0,0 +1,8 @@
+package BmiService;
+
+public class BmiService {
+    public int calculate(int weightInKg, double heightInMeters) {
+        double bmi = weightInKg / heightInMeters / heightInMeters;
+        return (int) bmi;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import BmiService.BmiService;
+
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weightInKg = 98;
+        double heightInMeters = 1.87;
+        int bmi = service.calculate(weightInKg, heightInMeters);
+        System.out.println(bmi);
+    }
+}
